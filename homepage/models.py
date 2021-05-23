2b0c5bf9a88c44dc1f9327e0118d198aa909e0e5
adityababar07 <adityababar715@gmail.com>
2021-05-23 09:47:03 +0530

    initiall changes

--- homepage/models.py
+++ homepage/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from mega import Mega
-
 # Create your models here.
 
 
@@ -9,7 +7,7 @@ class Project(models.Model):
     project_id = models.AutoField(primary_key=True)
     project_name = models.CharField(max_length=50)
     project_description = models.TextField(max_length=500)
-    project_image = models.ImageField(upload_to='project_screenshot')
+    project_image = models.ImageField(upload_to='project_screenshots')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -18,8 +16,4 @@ class Project(models.Model):
 
     def get_absolute_url(self):
         return reverse('project_detail', args=[str(self.pk)])
-    
-    def upload(self):
-        mega = Mega()
-        m = mega.login('adityababar715@gmail.com', 'Nidhi@149489')
-        
+
