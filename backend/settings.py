8783787764b68de6c0592d80e3d931bbe6eba8c2
hacker07 <hacker07@localhost.localdomain>
2023-01-15 05:14:08 +0530

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mywebsite',
         'USER': 'adityababar07',
         'PASSWORD' : '149489',
