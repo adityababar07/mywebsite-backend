ea1f791887291d96a447b23e71d178e98fa7c33a
hacker07 <hacker07@localhost.localdomain>
2023-01-15 03:51:29 +0530

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm1)0w7v87xj-ok4m5%$k65xe=8ig20-1ae002y53kc4a+5q^c6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mywebsite-backend-qa5jdlawo-adityababar07.vercel.app','mywebsite-backend.vercel.app','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh','mywebsite-backend-qa5jdlawo-adityababar07.vercel.app','mywebsite-backend.vercel.app','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -143,7 +143,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
