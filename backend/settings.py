82867bd21e733d59a9e39d8f7cde18cb2399a65f
hacker07 <hacker07@localhost.localdomain>
2023-01-15 06:23:34 +0530

    initial

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'm1)0w7v87xj-ok4m5%$k65xe=8ig20-1ae002y53kc4a+5q^c6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-production-66ba.up.railway.app', '.railway.app','.vercel.app', '.now.sh','mywebsite-backend-qa5jdlawo-adityababar07.vercel.app','mywebsite-backend.vercel.app','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['adityababar07.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
